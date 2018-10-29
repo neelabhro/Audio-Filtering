a18092d8fa3a5c017f60c6d25d49114a5387091e
Neelabhro Roy <neelabhro16171@iiitd.ac.in>
2018-10-30 00:58:39 +0530

    Update dsp41.m

--- dsp41.m
+++ dsp41.m
@@ -30,20 +30,20 @@ title('Input Recording');
 figure;
 
 %Filtering
-b = [.5 .5];
-a=[0.5 0.25];
+b = [0.25 0.5 0.25];
+a=[1];
 %n= 0:200;
 %x = .1.^n;
 % input is x
 x = myRecording;
 
-d1 = fdesign.lowpass('N,Fc',10,1200,8000);
+%d1 = fdesign.lowpass('N,Fc',10,1200,8000);
 % We are performing Lowpass filtering at the Half Bandwidth
-designmethods(d1);
+%designmethods(d1);
 
-f1 = design(d1, 'window');  
+%f1 = design(d1, 'window');  
 %fvtool(f);
-y = filter(f1,x);
+y = filter(b,a,x);
 
 
 %y = filter(b,a,x);
@@ -65,15 +65,17 @@ figure;
 
 %Question 2
 
-d2 = fdesign.highpass('N,Fc',10,1200,8000);
+%d2 = fdesign.highpass('N,Fc',10,1200,8000);
 %('Fp,Fst,Ap,Ast',0.0001,0.1,1,60);
 % We are doing Highpass Window filtering of the input signal, and as is
 % evident from the resultant Sound, only the High frequencies above the
 % Half Bandwidth are resolved and audible.
-designmethods(d2);
-f2 = design(d2, 'window');
+%designmethods(d2);
+%f2 = design(d2, 'window');
+b1 = [0.25 -0.5 0.25];
+a1 = [1];
 
-i = filter(f2,y);
+i = filter(b1,a1,x);
 plot(i);
 title('High Pass Filtered Output');
 hold on
@@ -95,7 +97,7 @@ title('Magnitude Response of the High Pass filter');
 figure;
 n = 1024;
 %N = length(y);
-FFT = fft(x,n);
+FFT = fft(x(1:1000),n);
 stem(abs(FFT));
 %Normalising the Fourier Transform
 %Fn = Fs/2; 
